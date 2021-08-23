ff3426863202d4800e8c2f61563016fd5d9e79a8
Dionne Stratton <mylife33@hotmail.com>
2021-08-23 18:18:01 -0500

    mvp

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,17 @@
-import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION } from './../actions';
+import {
+  ADD_ONE,
+  APPLY_NUMBER,
+  CHANGE_OPERATION,
+  CLEAR_DISPLAY,
+  MEMORY_PLUS,
+  MEMORY_CLEAR,
+  MEMORY_RECALL,
+} from "./../actions";
 
 export const initialState = {
-    total: 100,
+    total: 0,
     operation: "*",
-    memory: 100
+    memory: 0
 }
 
 const calculateResult = (num1, num2, operation) => {
@@ -18,27 +26,48 @@ const calculateResult = (num1, num2, operation) => {
 }
 
 const reducer = (state, action) => {
-    switch(action.type) {
-        case(ADD_ONE):
-            return({
-                ...state,
-                total: state.total + 1
-            });
+    switch (action.type) {
+      case ADD_ONE:
+        return {
+          ...state,
+          total: state.total + 1,
+        };
 
-        case(APPLY_NUMBER):
-            return ({ 
-                ...state, 
-                total: calculateResult(state.total, action.payload, state.operation)
-            });
-        
-        case(CHANGE_OPERATION):
-            return ({
-                ...state,
-                operation: action.payload
-            });
-            
-        default:
-            return state;
+      case APPLY_NUMBER:
+        return {
+          ...state,
+          total: calculateResult(state.total, action.payload, state.operation),
+        };
+
+      case CHANGE_OPERATION:
+        return {
+          ...state,
+          operation: action.payload,
+        };
+
+      case CLEAR_DISPLAY:
+        return {
+          ...state,
+          total: 0,
+        };
+      case MEMORY_PLUS:
+        return {
+          ...state,
+          memory: state.total,
+        };
+      case MEMORY_CLEAR:
+        return {
+          ...state,
+          memory: 0,
+        };
+      case MEMORY_RECALL:
+        return {
+          ...state,
+          total: calculateResult(state.total, state.memory, state.operation),
+        };
+
+      default:
+        return state;
     }
 }
 
